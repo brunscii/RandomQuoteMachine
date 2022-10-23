78df1dd3bafb4bcd1ce46ae0014268509f2bb781
Chris Carlin <brunscii@gmail.com>
2022-10-23 04:21:37 -0400

    added quote api request and added even listener

---
+++ index.js
@@ -0,0 +1,40 @@
+const next = document.getElementById('next-quote');
+const colors = ['#5465DA','#DB72A0','#5CDBD9','#DBB946','#DA6E37','#84C974']
+
+function changeColor(color){
+    document.body.style.backgroundColor = color;
+}
+
+function randomColor(){
+    document.documentElement.style.setProperty('--newColor',colors[Math.floor(Math.random()*100%colors.length)])
+    // document.body.style.backgroundColor = colors[Math.floor(Math.random()*100%colors.length)];
+}
+// setInterval(randomColor,5000);
+async function updateQuote() {
+    
+    const quote = document.documentElement.querySelector("#quote");
+    const author = document.documentElement.querySelector('#author');
+    
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    
+    if (response.ok) {
+        
+        quote.textContent = data.content;
+        author.textContent = data.author;
+        
+        console.log(data.content);
+    } else {
+        
+        quote.textContent = "An error occured";
+        console.log(data);
+    
+    }
+
+    randomColor()
+}
+onload = () =>{
+    updateQuote();
+    document.getElementById('next-quote').addEventListener("click", updateQuote );
+}
+// setInterval(updateQuote,10000);
